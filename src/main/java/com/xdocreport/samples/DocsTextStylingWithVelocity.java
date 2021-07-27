6201d7a880a61d7ab97e1dadb0ceff2a2d6ad9d8
Alex Chamba <alexjhon@live.com>
2021-07-26 20:46:08 -0500

    [ADD]: Resources are added to the project.

--- src/main/java/com/xdocreport/samples/DocsTextStylingWithVelocity.java
+++ src/main/java/com/xdocreport/samples/DocsTextStylingWithVelocity.java
@@ -14,8 +14,8 @@ import fr.opensagres.xdocreport.template.IContext;
 import fr.opensagres.xdocreport.template.TemplateEngineKind;
 import fr.opensagres.xdocreport.template.formatter.FieldsMetadata;
 import fr.opensagres.xdocreport.core.document.SyntaxKind;
-//import fr.opensagres.xdocreport.document.docx.DocxConstants;
-//import fr.opensagres.xdocreport.document.docx.preprocessor.dom.DOMFontsPreprocessor;
+import fr.opensagres.xdocreport.document.docx.DocxConstants;
+import fr.opensagres.xdocreport.document.docx.preprocessor.dom.DOMFontsPreprocessor;
 
 /**
  * Allows you to insert data with or without HTML tags into documents in .odt or .docx format. 
@@ -30,8 +30,7 @@ public class DocsTextStylingWithVelocity {
     public static void main(String[] args) {
 
         try {
-            //String pathDocIn = "/home/alexjcm/Descargas/DOCXHtmlTextStyle.docx";
-            String pathDocIn = "/home/alexjcm/Descargas/ODTHtmlTextStyle.odt";
+            String pathDocIn = "src/main/resources/DocxHTMLTextStylingWithVelocity.docx";      
             if (!(pathDocIn.endsWith(DOCX_EXT) || pathDocIn.endsWith(ODT_EXT))) {
                 throw new Exception("La plantilla debe ser un documento .docx o .odt, no se admiten otros formatos.");
             }
@@ -40,17 +39,19 @@ public class DocsTextStylingWithVelocity {
             File file = new File(pathDocIn);
             InputStream in = new FileInputStream(file);
             IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in, TemplateEngineKind.Velocity);
-            /*report.addPreprocessor( DocxConstants.WORD_DOCUMENT_XML_ENTRY, DOMFontsPreprocessor.INSTANCE );
-            report.addPreprocessor( DocxConstants.WORD_HEADER_XML_ENTRY, DOMFontsPreprocessor.INSTANCE );
-            report.addPreprocessor( DocxConstants.WORD_FOOTER_XML_ENTRY, DOMFontsPreprocessor.INSTANCE );*/
+            //report.addPreprocessor( DocxConstants.WORD_DOCUMENT_XML_ENTRY, DOMFontsPreprocessor.INSTANCE );
+            //report.addPreprocessor( DocxConstants.WORD_HEADER_XML_ENTRY, DOMFontsPreprocessor.INSTANCE );
+            //report.addPreprocessor( DocxConstants.WORD_FOOTER_XML_ENTRY, DOMFontsPreprocessor.INSTANCE );
            
             // 2) Create fields metadata to manage text styling
             FieldsMetadata metadata = report.createFieldsMetadata();
-            metadata.addFieldAsTextStyling("comments_html", SyntaxKind.Html);
+            metadata.addFieldAsTextStyling("comments", SyntaxKind.Html);
 
             // 3) Create context Java model
             IContext context = report.createContext();            
-            context.put("comments_html", "Here are severals styles :<ul>" + "<li><strong>Bold</strong> style.</li>"
+            context.put("comments", "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+                    + "when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting," 
+                    + "remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><ul>" + "<li><strong>Bold</strong> style.</li>"
                     + "<li><em>Italic</em> style.</li>" + "<li><strong><em>BoldAndItalic</em></strong> style.</li>"
                     + "</ul>" + "<p>Here are 3 styles :</p>" + "<ol>" + "<li><strong>Bold</strong> style.</li>"
                     + "<li><em>Italic</em> style.</li>" + "<li><strong><em>BoldAndItalic</em></strong> style.</li>"
@@ -59,7 +60,7 @@ public class DocsTextStylingWithVelocity {
                     + "Now some <strong>headers</strong>:" + "</p>" + "<h1>Title 1</h1>" + "<p>Some text...</p>"
                     + "<h2>Title 2</h2>" + "<p>Some text...</p>" + "<h3>Title 3</h3>" + "<p>Some text...</p>");
             //context.put(DOMFontsPreprocessor.FONT_NAME_KEY, "Magneto");
-            //context.put(DOMFontsPreprocessor.FONT_SIZE_KEY, 40);
+            //context.put(DOMFontsPreprocessor.FONT_SIZE_KEY, 25);
                    
             // 4) Generate report by merging Java model with the DOC
             String pathDocOut = pathDocIn.replace(".", "_out.");
