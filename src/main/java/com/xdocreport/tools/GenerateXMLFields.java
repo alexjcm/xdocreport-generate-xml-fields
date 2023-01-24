23e5a7c1500596c16aa6389fbc93a133ab4833b0
Alex John Chamba <achamba@ec.krugercorp.com>
2023-01-23 20:32:50 -0500

    [REF] Se agrega nuevos campos

--- src/main/java/com/xdocreport/tools/GenerateXMLFields.java
+++ src/main/java/com/xdocreport/tools/GenerateXMLFields.java
@@ -24,7 +24,20 @@ public class GenerateXMLFields {
         fieldsMetadata.addFieldAsTextStyling("cn_cabeceraDelCertificado", SyntaxKind.Html);
         fieldsMetadata.addFieldAsTextStyling("cn_cuerpoDelCertificado", SyntaxKind.Html);
 
-        // Add fields metadata           
+        // Add fields metadata
+        // SOLICITUDES
+        fieldsMetadata.addField("requestCode", false, "", "", false);
+        fieldsMetadata.addField("s_numeroDeProrroga", false, "", "", false);
+        fieldsMetadata.addField("s_tituloTrabajoTitulacion", false, "", "", false);
+        fieldsMetadata.addField("s_directorTrabajoTitulacion", false, "", "", false);
+        fieldsMetadata.addField("s_mesYearInicioPeriodoAcademico", false, "", "", false);
+        fieldsMetadata.addField("s_mesYearFinPeriodoAcademico", false, "", "", false);
+        fieldsMetadata.addField("s_nombreDelCertificado", false, "", "", false);
+        fieldsMetadata.addField("s_motivoSolicitudCertificado", false, "", "", false);
+
+        // CERTIFICADOS
+        fieldsMetadata.addField("nombreDeLaCarrera", false, "", "", false);
+        fieldsMetadata.addField("nombreDeLaCarreraAux", false, "", "", false);
         fieldsMetadata.addField("nombreDelEstudiante", false, "", "", false);
         fieldsMetadata.addField("cedulaDelEstudiante", false, "", "", false);
         fieldsMetadata.addField("fechaDeEmision", false, "", "", false);
@@ -45,8 +58,9 @@ public class GenerateXMLFields {
 
         fieldsMetadata.addField("c2_fechaInicioDeEstudios", false, "", "", false);
         fieldsMetadata.addField("c2_fechaFinDeEstudios", false, "", "", false);
-        fieldsMetadata.addField("c2_fechaInicioPrimeraProrroga", false, "", "", false);
-        fieldsMetadata.addField("c2_fechaFinPrimeraProrroga", false, "", "", false);
+        fieldsMetadata.addField("c2_numeroDeProrroga", false, "", "", false);
+        fieldsMetadata.addField("c2_fechaInicioProrroga", false, "", "", false);
+        fieldsMetadata.addField("c2_fechaFinProrroga", false, "", "", false);
 
         fieldsMetadata.addField("c3_fechaInicioDeEstudios", false, "", "", false);
         fieldsMetadata.addField("c3_fechaFinDeEstudios", false, "", "", false);
@@ -61,10 +75,11 @@ public class GenerateXMLFields {
         fieldsMetadata.addField("c4_fechaInicioDelEvento", false, "", "", false);
         fieldsMetadata.addField("c4_fechaFinDelEvento", false, "", "", false);
         fieldsMetadata.addField("c4_horasDelEvento", false, "", "", false);
+        fieldsMetadata.addField("c4_tipoCertEvento", false, "", "", false);
 
         // 3) Generate XML fields in the file "*.fields.xml".
         // Extension *.fields.xml is very important to use it with MS Macro XDocReport.dotm
-        String pathFile = "/home/ajcm/Downloads/certificados.fields.xml";
+        String pathFile = "/home/ajcm/Downloads/solicitudes_y_certificados.fields.xml";
         if (!pathFile.endsWith(".fields.xml")) {
             throw new Exception("The file must end with the extension .fields.xml");
         }
